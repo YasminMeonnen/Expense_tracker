16610f022ff486fcca8e3cec7dcfcbc426c04f08
YasminMeonnen <yasminmekonnen@gmail.com>
2025-04-26 12:43:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ else:
 # Show total
 st.subheader("Total Spent")
 st.write(f"${total_expenses(st.session_state.expenses)}")
-if remaining >= 0:
+
     st.success(f"You're within budget. You have ${remaining:.2f} remaining.")
 else:
     st.error(f"You have exceeded your budget by ${abs(remaining):.2f}!")
