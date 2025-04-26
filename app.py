ac764cc63b9a0172e3515f022e467d5eba47df71
YasminMeonnen <yasminmekonnen@gmail.com>
2025-04-26 12:32:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ with st.form("expense_form"):
     category = st.text_input("Category")
     date = st.date_input("Date") 
     budget = st.number_input("Set your Budget")
-    budget - expenses = (st.success() , st.error()) 
+    budget - expenses == (st.success() , st.error()) 
      
     submitted = st.form_submit_button("Add Expense")
     if submitted:
